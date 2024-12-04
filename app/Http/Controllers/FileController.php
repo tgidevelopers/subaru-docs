a409a10ddb4f3572681ed5ccd8d4bb1f3de6757d
isigillo <isigillo@tweddle.com>
2024-12-04 11:56:11 +0100

    first commit

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class FileController extends Controller
+{
+    public function index(Request $request) {
+        if ($request->input('path')) {
+            $inputDirectory = $request->input('path');
+        } else {
+            $inputDirectory="doc";
+        }
+
+        $directories = Storage::directories($inputDirectory);
+        $files = Storage::files($inputDirectory);
+        $fileReport = collect();
+        foreach ($directories as $directory) {
+            $localfiles = Storage::files($directory);
+            $count=count($localfiles);
+            $item=[
+                "dir"=>$directory,
+                "count"=>$count,
+            ];
+            $fileReport->push($item);
+            //Storage::disk('local')->append('doc/report.csv', $directory.','.$count);
+        }
+        //$path=Storage::disk('local')->put("doc/report.json", json_encode($fileReport));
+        return view('files', ['directories'=>$fileReport, 'files'=>$files]);
+    }
+}
---
+++ resources/views/files.blade.php
@@ -0,0 +1,21 @@
+<table>
+    @foreach($directories as $file)
+        <tr>
+            <td style="background-color: #e5e7eb;"><a
+                    href="{{route('list', ['path' => $file['dir']])}}">{{$file['dir']}}</a></td>
+            <td style="text-align: right;">{{$file['count']}}</td>
+        </tr>
+    @endforeach
+    @foreach($files as $filename)
+        <tr>
+            <td>
+                <a href="{{\Illuminate\Support\Facades\Storage::temporaryUrl($filename,now()->addMinutes(5))}}" target="_blank">{{$filename}}</a>
+            </td>
+            <td></td>
+        </tr>
+    @endforeach
+    {{--<tr>
+        <td>FILE</td>
+        <td style="text-align: right;">{{count($files)}}</td>
+    </tr>--}}
+</table>
