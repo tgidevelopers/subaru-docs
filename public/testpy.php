70577bf39763d92c822ffaf08cf733ce18a83210
Alessandro Succodato <51737556+alessandrosuccodato@users.noreply.github.com>
2024-12-11 01:35:45 +0100

    Create testpy.php

---
+++ public/testpy.php
@@ -0,0 +1,10 @@
+< ?php
+// To Run the Python program 
+$command = escapeshellcmd('python3 /home/ec2-user/script/.main.py');
+
+// Use shell_exec to execute the command and capture the output
+$output = shell_exec($command);
+
+// Display the output
+echo $output;
+? >
